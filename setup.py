e1dc910347da45ff450f46e5390f5fcbe90a29ec
Franck Chauvel <franck.chauvel@gmail.com>
2015-08-13 10:39:59 +0200

    Adding a setup.py to help with installation and packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='FLaP',
+     version='0.1',
+     description='Flat LaTeX Projects',
+     author='Franck Chauvel',
+     author_email='franck.chauvel@gmail.com',
+     url='https://github.com/fchauvel/flap',
+     packages=['flap']
+     )
