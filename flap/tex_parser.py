66680f0f32218bdf1b2abb0f76f19f8c65cfe9b8
Franck Chauvel <franck.chauvel@gmail.com>
2016-09-14 15:23:42 +0200

    Adding expansion of multi parameters macros

--- flap/tex_parser.py
+++ flap/tex_parser.py
@@ -15,21 +15,53 @@
 # along with Flap.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from re import sub
 from itertools import chain
 
 
 class Macro:
 
-    def __init__(self, name):
+    def __init__(self, name, parameters, replacement):
         self._name = name
-        self._parameters = []
+        self._parameters = parameters
+        self._replacement = replacement
 
     @property
     def name(self):
         return self._name
 
-    def has_parameters(self):
-        return len(self._parameters) > 0
+    def evaluate(self, interpreter, tokens):
+        replacement = self._replacement
+        if self._has_parameters():
+            parameters = tokens.extract_parameters(self._parameters)
+            replacement = self._substitute(parameters)
+        interpreter.typeset(replacement)
+
+    def _has_parameters(self):
+        return self._parameters != [""]
+
+    def _substitute(self, parameters):
+        body = self._replacement
+        for (key, value) in parameters.items():
+            pattern = r"#%d" % key
+            body = sub(pattern, value, body)
+        return body
+
+    def __eq__(self, other):
+        if not isinstance(other, Macro): return False
+        return other._name == self._name \
+            and other._parameters == self._parameters \
+            and other._replacement == self._replacement
+
+
+class DefineMacro(Macro):
+
+    def __init__(self):
+        super().__init__("def", [], "")
+
+    def evaluate(self, interpreter, tokens):
+        macro = tokens.capture_macro_definition()
+        interpreter.define(macro)
 
 
 class TokenStream:
@@ -46,8 +78,38 @@ class TokenStream:
     def prepend(self, text):
         self._stream = chain(iter(text), self._stream)
 
-    def extract_macro_name(self):
+    def capture_macro_definition(self):
+        assert next(self).starts_a_macro()
+        macro_name = self._capture_macro_name()
+        parameters = self.extract_macro_parameters()
+        replacement = self.extract_replacement()
+        assert next(self).ends_replacement()
+        return Macro(macro_name, parameters, replacement)
+
+    def _capture_macro_name(self):
         return self._extract_while(lambda token: token.belongs_to_the_macro_name())
+
+    def extract_parameters(self, arguments):
+        parameters = dict()
+        for index in range(len(arguments)):
+            each_argument = arguments[index]
+            if each_argument.startswith("#"):
+                key = int(each_argument[1:])
+                if index+1 < len(arguments):
+                    next_argument = arguments[index+1]
+                    parameters[key] = self._extract_until_pattern(next_argument)
+                else:
+                    parameters[key] = self._extract_until(lambda token: token.starts_a_macro())
+            else:
+                self.extract(each_argument)
+        return parameters
+
+    def extract(self, text):
+        for each_character in text:
+            found = str(next(self))
+            if each_character != found:
+                raise UnexpectedToken(found, each_character)
+        return text
 
     def _extract_while(self, shall_include):
         buffer = ""
@@ -59,8 +121,21 @@ class TokenStream:
                 return buffer
         return buffer
 
+    def _extract_until_pattern(self, pattern):
+        buffer = ""
+        for each_token in self:
+            buffer += str(each_token)
+            if buffer.endswith(pattern):
+                self.prepend(pattern)
+                return buffer[:len(buffer)-len(pattern)]
+        raise UnexpectedToken(expected=pattern, actual="End of stream")
+
     def _extract_until(self, accept):
-        return self._extract_while(lambda token: not accept(token))
+        if isinstance(accept, str):
+            return self._extract_while(lambda token: str(token) != accept)
+        if callable(accept):
+            return self._extract_while(lambda token: not accept(token))
+        return ValueError("Invalid type of acceptance criteria (found: %s)" % type(accept))
 
     def extract_macro_parameters(self):
         parameters = [""]
@@ -123,33 +198,33 @@ class TeXInterpreter:
         self._environment = environment
         self._output = output
 
+    def typeset(self, text):
+        self._output.write(text)
+
     def evaluate(self, input):
         tokens = TokenStream(input)
         for any_token in tokens:
             if any_token.starts_a_macro():
-                (parameters, replacement) = self._match_macro(tokens)
-                tokens.prepend(replacement)
+                macro = self._match_macro(tokens)
+                macro.evaluate(self, tokens)
             else:
-                self._output.write(str(any_token))
+                self.typeset(str(any_token))
 
     def _match_macro(self, tokens):
-        name = tokens.extract_macro_name()
-        if name == "def":
-            self._define(tokens)
-            return ([], "")
-        else:
-            if name in self._environment:
-                return self._environment[name]
-            raise UnknownMacroException(name)
+        name = tokens._capture_macro_name()
+        if name in self._environment:
+            return self._environment[name]
+        raise UnknownMacroException(name)
 
-    def _define(self, tokens):
-        assert next(tokens).starts_a_macro()
-        macro_name = tokens.extract_macro_name()
-        parameters = tokens.extract_macro_parameters()
-        replacement = tokens.extract_replacement()
-        assert next(tokens).ends_replacement()
-        self._environment[macro_name] = (parameters, replacement)
+    def define(self, macro):
+        self._environment[macro.name] = macro
 
+
+class UnexpectedToken(Exception):
+
+    def __init__(self, actual, expected):
+        self._expected = expected
+        self._actual = actual
 
 class UnknownMacroException(Exception):
 
--- tests/unit/tex_parser.py
+++ tests/unit/tex_parser.py
@@ -19,16 +19,34 @@
 from unittest import TestCase, main
 from io import StringIO
 
-from flap.tex_parser import TeXInterpreter, Macro, UnknownMacroException
+from flap.tex_parser import TeXInterpreter, TokenStream, Macro, DefineMacro, UnknownMacroException, UnexpectedToken
 
 
 class MacroTest(TestCase):
 
     def setUp(self):
-        self.macro = Macro("foo")
+        self.macro = Macro("foo", [], "bar")
 
     def test_macro_expose_their_name(self):
         self.assertEqual("foo", self.macro.name)
+
+
+class TokenStreamTests(TestCase):
+
+    def test_extract_until_find_word(self):
+        tokens = TokenStream("abcde---bbb")
+        actual = tokens._extract_until_pattern("---")
+        self.assertEqual("abcde", actual)
+
+    def test_extract_until_avoids_prefix(self):
+        tokens = TokenStream("ab-cde---bbb")
+        actual = tokens._extract_until_pattern("---")
+        self.assertEqual("ab-cde", actual)
+
+    def test_extract_until_detects_missing_patterns(self):
+        tokens = TokenStream("abcde---bbb")
+        with self.assertRaises(UnexpectedToken):
+            actual = tokens._extract_until_pattern("zzzz")
 
 
 class TeXParserTests(TestCase):
@@ -37,6 +55,7 @@ class TeXParserTests(TestCase):
         self.environment = dict()
         self.output = StringIO()
         self.tex = TeXInterpreter(self.environment, self.output)
+        self.tex.define(DefineMacro())
 
     def test_typesetting_a_text(self):
         tex_input = "This is a text!"
@@ -49,21 +68,37 @@ class TeXParserTests(TestCase):
             self.tex.evaluate(r"\name")
 
     def test_typesetting_a_macro(self):
-        self.environment["name"] = ([""], "franck")
+        self.tex.define(Macro("name", [""], "franck"))
         self.tex.evaluate(r"Hello \name")
         self.assertEqual("Hello franck", self.output.getvalue())
 
     def test_definition_macro(self):
         self.tex.evaluate(r"\def\hello{Hi Guys!}")
-        self.assertEqual(([""], "Hi Guys!"), self.environment["hello"])
+        self.assertEqual(Macro("hello", [""], "Hi Guys!"), self.environment["hello"])
 
-    def test_usage_of_a_macro(self):
-        self.tex.evaluate("""\def\hello{Hi Guys!}\hello""")
+    def test_expanding_a_macro(self):
+        self.tex.define(Macro("hello", [""], "Hi Guys!"))
+        self.tex.evaluate(r"\hello")
         self.assertEqual("Hi Guys!", self.output.getvalue())
 
-    def test_definition_of_a_macro_with_one_parameter(self):
+    def test_defining_a_one_parameter_macro(self):
         self.tex.evaluate(r"\def\value(#1){The value is '#1'!}")
-        self.assertEquals((["(", "#1", ")"], "The value is '#1'!"), self.environment[r"value"])
+        self.assertEquals(Macro("value", ["(", "#1", ")"], "The value is '#1'!"), self.environment["value"])
+
+    def test_expanding_a_one_parameter_macro(self):
+        self.tex.define(Macro("value", ["(", "#1", ")"], "The value is '#1'!"))
+        self.tex.evaluate(r"\value(123)")
+        self.assertEqual("The value is '123'!", self.output.getvalue())
+
+    def test_expanding_a_one_parameter_macro_with_a_different_syntax(self):
+        self.tex.define(Macro("value", ["[", "#1", "]"], "The value is '#1'!"))
+        self.tex.evaluate(r"\value[123]")
+        self.assertEqual("The value is '123'!", self.output.getvalue())
+
+    def test_expanding_a_two_parameters_macro(self):
+        self.tex.define(Macro("values", ["#1", "---", "#2"], "The values are '#1' and '#2'!"))
+        self.tex.evaluate(r"\values123---234")
+        self.assertEqual("The values are '123' and '234'!", self.output.getvalue())
 
 if __name__ == "__main__":
     main()
