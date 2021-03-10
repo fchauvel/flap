5c3efc81cce9004f30c84c90c001961c77c95aa2
Franck Chauvel <franck.chauvel@sintef.no>
2021-03-10 16:02:40 +0100

    fix: Detailed debugging information in the log file

--- flap/latex/parser.py
+++ flap/latex/parser.py
@@ -17,11 +17,11 @@
 # along with Flap.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-
+from flap import logger
 from flap.latex.commons import Stream, Source
 from flap.latex.lexer import Lexer
 from flap.latex.errors import UnknownSymbol
-
+from flap.util import truncate
 
 class Context:
 
@@ -73,9 +73,11 @@ class Parser:
         self._create = factory
         self._tokens = self._create.as_stream(tokens)
         self._definitions = environment
+        self._level = 0
 
     def _spawn(self, tokens, environment):
-        new_environment = Context(self._definitions, definitions=environment)
+        new_environment = Context(self._definitions,
+                                  definitions=environment)
         parser = Parser(
             tokens,
             self._create,
@@ -101,10 +103,13 @@ class Parser:
             return [self._tokens.take()]
 
     def _rewrite_group(self):
+        self._level += 1
         tokens = self._accept(lambda token: token.begins_a_group)
         while not self._next_token.ends_a_group:
             tokens += self._rewrite_one()
         tokens += self._accept(lambda token: token.ends_a_group)
+        self._debug("group", "rewritten", tokens)
+        self._level -= 1
         return tokens
 
     def _rewrite_command(self):
@@ -116,7 +121,8 @@ class Parser:
 
     @property
     def _next_token(self):
-        return self._tokens.look_ahead()
+        next_token = self._tokens.look_ahead()
+        return next_token
 
     def default(self, text):
         return [self._tokens.take()]
@@ -137,7 +143,8 @@ class Parser:
         self._raise_unexpected_token()
 
     def _raise_unexpected_token(self):
-        error = "Unexpected {} '{}' in file {} (line {}, column {}).".format(
+        error = "Unexpected {} '{}' in file {} (line {}, column {})."\
+        .format(
             self._next_token._category.name,
             self._next_token,
             self._next_token.location.source,
@@ -181,6 +188,7 @@ class Parser:
         self._accept(lambda token: token.begins_a_group)
         tokens = self._evaluate_until(lambda token: token.ends_a_group)
         self._accept(lambda token: token.ends_a_group)
+        self._debug("group", "evaluated", tokens)
         return tokens
 
     def _evaluate_until(self, is_excluded):
@@ -206,12 +214,14 @@ class Parser:
             result += self._evaluate_until(lambda token: token.has_text(end))
             result += self._accept(lambda token: token.has_text(end))
             result += self._tokens.take_while(lambda c: c.is_ignored)
+        self._debug("options", "capture", result)
         return result
 
     def capture_macro_name(self, name=None):
         tokens = self._accept(lambda token: token.is_a_command)
         if name and not tokens[-1].has_text(name):
             self._raise_unexpected_token()
+        self._debug("macro", "captured", tokens)
         return tokens
 
     def capture_ignored(self):
@@ -240,6 +250,7 @@ class Parser:
         while not self._next_token.ends_a_group:
             tokens += self.capture_one()
         tokens += self._accept(lambda token: token.ends_a_group)
+        self._debug("group", "captured", tokens)
         return tokens
 
     def capture_one(self):
@@ -254,6 +265,7 @@ class Parser:
         return "".join(map(str, tokens))
 
     def shall_expand(self):
+        logger.debug("Expanding")
         result = False
         for _, any_macro in self._definitions.items():
             was_called = getattr(any_macro, "was_called", None)
@@ -266,3 +278,18 @@ class Parser:
             if was_called:
                 any_macro._called = False
         return result
+
+    def _debug(self, category, action, tokens):
+        text = ""
+        position = "None"
+        if not len(tokens) == 0:
+            text = truncate(self._as_text(tokens), 30)
+            position = tokens[-1].location,
+        logger.debug(
+                "{} (GL={}) '{}' {} {}".format(
+                    position,
+                    self._level,
+                    category,
+                    action,
+                    text
+                ))
